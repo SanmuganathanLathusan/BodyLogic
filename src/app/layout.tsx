f29ce9e92112ee113fbb72604ea41bfcc9b44bf4
SanmuganathanLathusan <sanmuganathanlathusan@gmail.com>
2026-05-11 21:17:35 +0530

    modified  src/app/layout.tsx file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col font-sans">
         <Providers>
           <Navbar />
-          <main className="flex-1 flex flex-col">
+          <main className="flex-1 flex flex-col pt-16">
             {children}
           </main>
         </Providers>
